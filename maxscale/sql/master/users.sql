ade5c8beb5c3c28d2cc29c87930448e53ea279e0
Nikolay Levitsky <nikolay.levitsky@ordergroove.com>
2019-02-04 13:31:59 -0500

    trying to get this to work

--- maxscale/sql/master/users.sql
+++ maxscale/sql/master/users.sql
@@ -1,6 +1,16 @@
 RESET MASTER;
 CREATE DATABASE test;
 
+CREATE TABLE test.pet (
+    ID int NOT NULL AUTO_INCREMENT,
+    name varchar(255) NOT NULL,
+    time varchar(255),
+    PRIMARY KEY (ID)
+);
+
+GRANT USAGE ON *.* TO 'niko'@'%' IDENTIFIED BY 'bob';
+GRANT ALL ON test.* TO 'niko'@'%';
+
 CREATE USER 'maxuser'@'127.0.0.1' IDENTIFIED BY 'maxpwd';
 CREATE USER 'maxuser'@'%' IDENTIFIED BY 'maxpwd';
 GRANT ALL ON *.* TO 'maxuser'@'127.0.0.1' WITH GRANT OPTION;
